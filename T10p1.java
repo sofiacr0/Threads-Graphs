e9b650081ea9709d5b85c95e69b262d2b4182ebd
sofiacr0 <100277513+sofiacr0@users.noreply.github.com>
2022-10-14 23:39:46 -0700

    second commit

--- T10p1.java
+++ T10p1.java
@@ -13,7 +13,7 @@ import java.awt.geom.*;
 public class T10p1 extends JComponent implements Runnable {
     Thread random;
     double d1ventas, rangoDep2, d2ventas, rangoDep3, d3ventas, rangoDep4, d4ventas;
-    int d1v,d2v,d3v,d4v;
+    double d1v,d2v,d3v,d4v;
     public T10p1(){
         random = new Thread(this);
         random.start();
@@ -22,10 +22,10 @@ public class T10p1 extends JComponent implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("Ventas de 4 departamentos (gráfica de pastel)");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBackground(Color.white);
+        frame.setBackground(new Color(240, 235, 227));
         frame.setSize(300, 300);
 
-        prueba pie = new prueba();
+        T10p1 pie = new T10p1();
         frame.add(pie);
         frame.setVisible(true);
     }
@@ -34,22 +34,31 @@ public class T10p1 extends JComponent implements Runnable {
     public void run() {
         while(true){
             //valores de ventas aleatorias en un rango de 100 a 1500
-            d1v = (100+(int)(Math.random()*((1500-100)+1)));
-            d2v = (100+(int)(Math.random()*((1500-100)+1)));
-            d3v = (100+(int)(Math.random()*((1500-100)+1)));
-            d4v = (100+(int)(Math.random()*((1500-100)+1)));
+            d1v = (100+(Math.random()*((1500-100)+1)));
+            d2v = (100+(Math.random()*((1500-100)+1)));
+            d3v = (100+(Math.random()*((1500-100)+1)));
+            d4v = (100+(Math.random()*((1500-100)+1)));
+            double sumtot = (d1v+d2v+d3v+d4v);
             //rebanada 1 porcentaje
-            d1ventas = (d1v*100)/1500;
+                //d1ventas = (d1v*100)/1500;
+                //d1ventas = (d1v*100)/sumtot;
+                d1ventas=(d1v*360)/sumtot;
             //rebanada 2 porcentaje
-            d2ventas = (d2v*100)/1500;
+                //d2ventas = (d2v*100)/1500;
+                //d2ventas = (d2v*100)/sumtot;
+                d2ventas=(d2v*360)/sumtot;
             ////rebanada 3 porcentaje
-            d3ventas = (d3v*100)/1500;
+                //d3ventas = (d3v*100)/1500;
+                //d3ventas = (d3v*100)/sumtot;
+                d3ventas=(d3v*360)/sumtot;
             //rebanada 4 porcentaje
-            d4ventas = (d4v*100)/1500;
+                //d4ventas = (d4v*100)/1500;
+                //d4ventas = (d4v*100)/sumtot;
+                d4ventas=(d4v*360)/sumtot;
             //calcular inicio de rebanada
-            rangoDep2 = (int) (0 + d1ventas);
-            rangoDep3 = (int) (rangoDep2 + d2ventas);
-            rangoDep4 = (int) (rangoDep3 + d3ventas);
+            rangoDep2 = (0 + d1ventas);
+            rangoDep3 = (rangoDep2 + d2ventas);
+            rangoDep4 = (rangoDep3 + d3ventas);
             repaint();
             try{
                 Thread.sleep(5000);
@@ -66,25 +75,25 @@ public class T10p1 extends JComponent implements Runnable {
 
         arc.setAngleStart(0);
         arc.setAngleExtent(d1ventas);
-        g2.setColor(Color.green);
+        g2.setColor(new Color(0, 110, 127));
         g2.draw(arc);
         g2.fill(arc);
 
         arc.setAngleStart(rangoDep2);
         arc.setAngleExtent(d2ventas);
-        g2.setColor(Color.blue);
+        g2.setColor(new Color(248, 203, 46));
         g2.draw(arc);
         g2.fill(arc);
 
         arc.setAngleStart(rangoDep3);
         arc.setAngleExtent(d3ventas);
-        g2.setColor(Color.red);
+        g2.setColor(new Color(238, 80, 7));
         g2.draw(arc);
         g2.fill(arc);
 
         arc.setAngleStart(rangoDep4);
         arc.setAngleExtent(d4ventas);
-        g2.setColor(Color.pink);
+        g2.setColor(new Color(178, 39, 39));
         g2.draw(arc);
         g2.fill(arc);
     }
